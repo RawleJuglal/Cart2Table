01bc635786cf2cbec62b345668d76c8c6ec330d2
Rawle Juglal <rawledjuglal@gmail.com>
2023-06-26 16:53:03 -0500

    feature
    added image for main page

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -3,6 +3,8 @@ import { onAuthStateChanged} from '@firebase/auth'
 import { auth } from '../../hooks/firebase'
 import { Outlet } from 'react-router-dom'
 import { C2TNavbar } from '../Navbar/C2TNavbar'
+import ShoppingCart from '../../assets/ShoppingCart.png'
+import './Layout.css'
 
 const Layout = ()=>{
     const [user, setUser] = useState(null)
@@ -16,6 +18,9 @@ const Layout = ()=>{
     return(
         <>
             <C2TNavbar user={user} />
+            <div className='--layout-image-container flex flex-center'>
+                <img className='--layout-shoppingCart-image' src={ShoppingCart} alt="picture of shopping Cart" />
+            </div>
             <Outlet context={user}/>
         </>
     )
